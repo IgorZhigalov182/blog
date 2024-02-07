9ca39d57a358f769439b788047ffd6baacd552d2
Igor <zhigalovzenit112233@gmail.com>
2024-02-07 14:29:20 +0300

    Add Theme context, hook for change theme, styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import { Counter } from './components/Counter';
-import './index.scss';
+import './styles/index.scss';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { useTheme } from './theme/useTheme';
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Switch theme</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
       <Suspense fallback={'Loading...'}>
