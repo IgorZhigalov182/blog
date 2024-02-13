bf58d2e4653636d17057f695418212860fe29d10
Igor <zhigalovzenit112233@gmail.com>
2024-02-13 22:45:02 +0300

    Add AppLink, Navbar, refactoring classNames

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,16 @@
-import { Link } from 'react-router-dom';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/Router';
+import { Navbar } from 'widgets/Navbar';
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
+      <Navbar />
       <button onClick={toggleTheme}>Switch theme</button>
-      <Link to="/">Главная</Link>
-      <Link to="/about">О сайте</Link>
       <AppRouter />
     </div>
   );
