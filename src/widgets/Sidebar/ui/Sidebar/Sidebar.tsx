95eacb1b0b293872b3c4ada1a864158e53ff9c15
Igor <zhigalovzenit112233@gmail.com>
2024-02-20 23:52:05 +0300

    Connected and configured i18n

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import cls from './Sidebar.module.scss';
 
 import { useState, type PropsWithChildren } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
+import { LangSwitcher } from 'widgets/LangSwitcher';
 
 interface SidebarProps {
   className?: string;
@@ -21,7 +22,7 @@ export const Sidebar = (props: PropsWithChildren<SidebarProps>) => {
       <button onClick={onToggle}>Toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        {/* {'Lang switcher'} */}
+        <LangSwitcher className={cls.lang} />
       </div>
     </div>
   );
