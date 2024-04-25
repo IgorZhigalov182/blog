6d1c0b1f50dbd4887040d86ff6032abe1e8cc4d3
Igor <zhigalovzenit112233@gmail.com>
2024-04-25 22:05:42 +0300

    Add TestAsyncThunk lib, add unit tests for LoginSlice, loginByUsername, login selectors

---
+++ src/features/AuthByUsername/model/selectors/getLoginUsername/getLoginUsername.test.ts
@@ -0,0 +1,15 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginUsername } from './getLoginUsername';
+
+describe('getLoginUsername.test', () => {
+  test('should return true', () => {
+    const state: DeepPartial<StateSchema> = { loginForm: { username: 'Igor' } };
+    expect(getLoginUsername(state as StateSchema)).toEqual('Igor');
+  });
+
+  test('should work with empty state', () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getLoginUsername(state as StateSchema)).toEqual(undefined);
+  });
+});
--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,10 +1,19 @@
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
+
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+    loginForm: loginReducer
+}
 
 export const StoreDecorator =
-    (initialState: DeepPartial<StateSchema>) => (StoryComponent: Story) => (
-        <StoreProvider initialState={initialState}>
-            <StoryComponent />
-        </StoreProvider>
-    );
+    (initialState: DeepPartial<StateSchema>,
+        asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>) =>
+        (StoryComponent: Story) => (
+            <StoreProvider
+                initialState={initialState}
+                asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
+                <StoryComponent />
+            </StoreProvider>
+        );
