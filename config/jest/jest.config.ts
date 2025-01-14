050946e906c1c23e4d0437f9340b9d2f5e72d25c
Igor Zhigalov <zhigalovzenit112233@gmail.com>
2025-01-14 15:00:35 +0300

    jest config fix

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -33,7 +33,7 @@ export default {
     [
       'jest-html-reporters',
       {
-        publicPath: '<rootDir>/report/unit',
+        publicPath: '<rootDir>/reports/unit',
         filename: 'report.html',
         // openReport: true,
         inlineSource: true,
